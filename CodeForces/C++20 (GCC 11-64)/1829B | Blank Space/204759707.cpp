e75e795bf859f97935066f9acfb0e8f28b378f00
Jack1eNgBaro <118097208+Jack1eNgBaro@users.noreply.github.com>
2024-06-16 20:05:55 +0700

    Committing files

---
+++ CodeForces/C++20 (GCC 11-64)/1829B | Blank Space/204759707.cpp
@@ -0,0 +1,46 @@
+/*
+  "There is no light without dark
+  There is no joy without pain
+  You can't have a rainbow without a little rain"
+         ~ Andrew Tate
+ 
+*/
+ 
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define ll long long
+#define pii pair<int,int>
+#define bit(x, i) ((x >> i) & 1)
+ 
+const int mod = 1e9 + 7;
+const int maxn = 1e6;
+const int base = 31;
+ 
+void file()
+{
+ ios_base::sync_with_stdio(0);
+ cin.tie(0); cout.tie(0);
+// freopen("input.txt", "r", stdin);
+// freopen("output.txt", "w", stdout);
+}
+ 
+int main()
+{
+ file();
+ int tc; cin >> tc;
+ while(tc--){
+  int n; cin >> n;
+  int a[n + 1]; 
+  for(int i=1;i<=n;i++) cin >> a[i];
+  
+  int ans = 0, cnt = 0;
+  for(int i=1;i<=n;i++){
+   if(a[i] == 0) ++cnt;
+   else cnt = 0;
+   ans = max(ans, cnt);
+  }
+  cout << ans << "\n";
+ }
+ return 0;
+}
