3b42c44ba6b033362bc121443978f938773f3dab
Jack1eNgBaro <118097208+Jack1eNgBaro@users.noreply.github.com>
2024-06-16 20:24:20 +0700

    Committing files

---
+++ CodeForces/C++20 (GCC 11-64)/688B | Lovely Palindromes/190940660.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define ll long long
+#define pii pair<int,int>
+ 
+const int mod = 1e9 + 7;
+const int maxn = 1e6;
+void file()
+{
+ ios_base::sync_with_stdio(0);
+ cin.tie(0); cout.tie(0);
+// freopen("input.txt", "r", stdin);
+// freopen("output.txt", "w", stdout);
+}
+ 
+int main()
+{
+ file();
+ string s; cin >> s;
+ string t = s;
+ reverse(t.begin(), t.end());
+ cout << s << t;
+ return 0;
+}
