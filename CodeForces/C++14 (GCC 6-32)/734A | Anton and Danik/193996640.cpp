57a6c707dfe2125134490d7936df13dbfc7e642d
Jack1eNgBaro <118097208+Jack1eNgBaro@users.noreply.github.com>
2024-06-16 20:19:57 +0700

    Committing files

---
+++ CodeForces/C++14 (GCC 6-32)/734A | Anton and Danik/193996640.cpp
@@ -0,0 +1,42 @@
+/*
+  "There is no light without dark
+  There is no joy without pain
+  You can't have a rainbow without a little rain"
+         ~ Andrew Tate
+ 
+*/
+ 
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define ll long long
+#define pii pair<int,int>
+#define bit(x, i) ((x >> i) & 1)
+ 
+const int mod = 1e9 + 7;
+const int maxn = 1e6;
+const int base = 31;
+ 
+void file()
+{
+ ios_base::sync_with_stdio(0);
+ cin.tie(0); cout.tie(0);
+// freopen("input.txt", "r", stdin);
+// freopen("output.txt", "w", stdout);
+}
+ 
+int main()
+{
+ file();
+ int n; cin >> n;
+ int a = 0, d = 0;
+ while(n--){
+  char c; cin >> c;
+  if(c == 'D') ++d;
+  else ++a;
+ }
+ if(a == d) cout << "Friendship";
+ else if(a > d) cout << "Anton";
+ else cout << "Danik";
+ return 0;
+}
