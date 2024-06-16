1b77f139a93aedb896d54feb2c05fb24b5f99338
Jack1eNgBaro <118097208+Jack1eNgBaro@users.noreply.github.com>
2024-06-16 20:40:05 +0700

    Committing files

---
+++ CodeForces/C++17 (GCC 7-32)/1772A | A+B?/185802180.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define ll long long
+#define pii pair<int,int>
+ 
+const int mod = 1e9 + 7;
+const int maxn = 1e6;
+void file()
+{
+ ios_base::sync_with_stdio(0);
+ cin.tie(0); cout.tie(0);
+// freopen("input.txt", "r", stdin);
+// freopen("output.txt", "w", stdout);
+}
+ 
+int main()
+{
+ file();
+ int tc; cin >> tc;
+ while(tc--)
+ {
+  char a, b, c; cin >> a >> b >> c;
+  int x = a - '0';
+  int y = c - '0';
+  cout << x + y << "\n";
+ }
+ return 0;
+}
