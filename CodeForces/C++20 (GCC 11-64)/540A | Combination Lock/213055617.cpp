c529f1a3172dcff916c4b0755e4814d809812b94
Jack1eNgBaro <118097208+Jack1eNgBaro@users.noreply.github.com>
2024-06-16 19:52:54 +0700

    Committing files

---
+++ CodeForces/C++20 (GCC 11-64)/540A | Combination Lock/213055617.cpp
@@ -0,0 +1,67 @@
+#include<bits/stdc++.h>
+#define MOD 1000000007
+#define fi first
+#define se second
+#define ll long long
+#define ii pair<int,int>
+#define Dennis ""
+#define heap_max(a) priority_queue<a>
+#define heap_min(a) priority_queue<a, vector<a>, greater <a>>
+#define all(x) (x).begin(), (x).end()
+#define pb push_back
+#define eb emplace_back
+#define el cout << '\n'
+#define rep(i, n) for(int i = 0; i < (n); i++)
+#define For(i, a, b) for(int i = (a); i <= (b); i++)
+#define Fod(i, a, b) for(int i = (a); i >= (b); i--)
+ 
+using namespace std;
+template <class X, class Y> bool minimize(X &a, Y b){
+    if (a > b) return a = b, true;
+    return false;
+}
+template <class X, class Y> bool maximize(X &a, Y b){
+    if (a < b) return a = b, true;
+    return false;
+}
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+int rnd(int l, int r) {return l + rng() % (r - l + 1);}
+const int N = 1e6 + 5;
+ 
+int cal(int a, int b){
+ int ans = 
+  min(abs(b-a), min(a+9-b+1, 9-a+b+1));
+ return ans;
+}
+ 
+void solve(){
+    int n; cin >> n;
+    string a, b; cin >> a >> b;
+    a = "~" + a;
+    b = "~" + b;
+ 
+    int ans = 0;
+    For(i,1,n){
+     int x = a[i] - '0';
+     int y = b[i] - '0';
+     ans += cal(x, y);
+     // cout << cal(x, y) << "\n"; 
+    }
+    cout << ans;
+}
+ 
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    int bla = uniform_int_distribution<int>(1, 100)(rng);
+    #define TASK ""
+    if(fopen(TASK".inp", "r")) {
+        freopen(TASK".inp", "r", stdin);
+        freopen(TASK".out", "w", stdout);
+    }
+    int T = 1;
+    //cin >> T;
+    while(T--){
+        solve();
+    }
+    return 0;
+}
