712aa3a54e7a949a51de6eff14e8dae42a8e9534
Jack1eNgBaro <118097208+Jack1eNgBaro@users.noreply.github.com>
2024-06-16 19:42:31 +0700

    Committing files

---
+++ CodeForces/C++20 (GCC 11-64)/276C | Little Girl and Maximum Sum/218668643.cpp
@@ -0,0 +1,69 @@
+#include<bits/stdc++.h>
+#pragma GCC optimize("O2")
+#pragma GCC target("avx,avx2,fma")
+#define MOD 1000000007
+#define fi first
+#define se second
+#define ll long long
+#define ii pair<int,int>
+#define Dennis ""
+#define heap_max(a) priority_queue<a>
+#define heap_min(a) priority_queue<a, vector<a>, greater <a>>
+#define all(x) (x).begin(), (x).end()
+#define pb push_back
+#define eb emplace_back
+#define el cout << '\n'
+#define rep(i, n) for(int i = 0; i < (n); i++)
+#define For(i, a, b) for(int i = (a); i <= (b); i++)
+#define Fod(i, a, b) for(int i = (a); i >= (b); i--)
+#define bit(x, i) ((x >> i) & 1)
+ 
+using namespace std;
+template <class X, class Y> bool minimize(X &a, Y b){
+    if (a > b) return a = b, true;
+    return false;
+}
+template <class X, class Y> bool maximize(X &a, Y b){
+    if (a < b) return a = b, true;
+    return false;
+}
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+int rnd(int l, int r) {return l + rng() % (r - l + 1);}
+const int N = 1e6 + 5;
+ 
+int n, m;
+int a[N], cnt[N];
+ 
+void solve(){
+    cin >> n >> m;
+    For(i,1,n) cin >> a[i];
+    For(i,1,m){
+     int x, y; cin >> x >> y;
+     cnt[x]++;
+     cnt[y+1]--;
+    }
+    For(i,1,n) cnt[i] += cnt[i-1];
+ 
+    sort(cnt + 1, cnt + n + 1);
+    sort(a + 1, a + n + 1);
+ 
+    ll ans = 0;
+    For(i,1,n) ans += 1ll * a[i] * cnt[i];
+    cout << ans;
+}
+ 
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    int bla = uniform_int_distribution<int>(1, 100)(rng);
+    #define TASK ""
+    if(fopen(TASK".inp", "r")) {
+        freopen(TASK".inp", "r", stdin);
+        freopen(TASK".out", "w", stdout);
+    }
+    int T = 1;
+    //cin >> T;
+    while(T--){
+        solve();
+    }
+    return 0;
+}
