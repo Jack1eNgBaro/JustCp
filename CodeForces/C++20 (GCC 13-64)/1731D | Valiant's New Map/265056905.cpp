7e42b1f340389f5ae0993f99edebe417c7cfb500
Jack1eNgBaro <118097208+Jack1eNgBaro@users.noreply.github.com>
2024-06-16 19:23:33 +0700

    Committing files

---
+++ CodeForces/C++20 (GCC 13-64)/1731D | Valiant's New Map/265056905.cpp
@@ -0,0 +1,121 @@
+ 
+/**
+TO MY NOTION:
+ 
+                                               ,----'   `-,
+                     ___,-'~~~`---'~~~~`-----' ,-'        \,                      
+             ___,---'          '        ,-~~~~~            :
+        _,--'                 ,        ; ;       ) "   __   \,
+   _,--'     ,                 ,'      :: "  ;  ` ,'  (\o\)  |
+  / _,       `,                     ,  `; " ;    (     `~~ `'\
+ ; /         ,`               ,     `    ~~~`. " ;   _     ,  `.
+,'/          ` ,              `     ` ,  ,    \_/ ?   ;    )   `.
+:;:            `                      `  ` ,     uu`--(___(    ~:
+:::          , ,  ,            ,   ;     , `  ,-'      \~(  ~   :
+||:          ` `  `         ,  ` ,'    ( ` _,-          \ \_   ~:
+:|`.        , ,  ,          `_   ;       ) );            \__>   :
+|:  : ;     ` `  ` ;  __,--~~ `-(         ( |              `.  ~|
+:|  :         ` __,--'           :  ()    : :               |~  ;
+|;  |  `     ,-'    ;             :  )(   | `.         /(   `. ~|
+::  :   ~  _/;     ;               |   )  :  :        ; /    ;~ ;
+{}  ;     /  :   ~ :               :      ; ,;        : `._,-  ,
+{} /~    /   ;    ;                 : ,  |  `;         `.___,-'
+  ;~    ;    ;  ~ `.                | `  )   ;
+  :`    \    `;~   \                ;~   `-, `-.     VOI24
+  `.__OOO;    `;_OOO;               )_____OO;_(O)
+   ~~~~~~       ~~~~                ~~~~~~~~ ~~~~
+   */
+ 
+ 
+#include<bits/stdc++.h>
+ 
+////Ta thuat
+//#pragma GCC optimize("O2")
+//#pragma GCC target("avx,avx2,fma")
+ 
+#define MOD 1000000007
+#define fi first
+#define se second
+#define ll long long
+#define ii pair<int,int>
+#define Dennis "Top1Server"
+#define Jack1e "DB bainao"
+#define heap_max(a) priority_queue<a>
+#define heap_min(a) priority_queue<a, vector<a>, greater <a>>
+#define all(x) (x).begin(), (x).end()
+#define pb push_back
+#define eb emplace_back
+#define el cout << '\n'
+#define rep(i, n) for(int i = 0; i < (n); i++)
+#define For(i, a, b) for(int i = (a); i <= (b); i++)
+#define Fod(i, a, b) for(int i = (a); i >= (b); i--)
+#define bit(x, i) ((x >> i) & 1)
+ 
+using namespace std;
+template <class X, class Y> bool minimize(X &a, Y b){
+    if (a > b) return a = b, true;
+    return false;
+}
+template <class X, class Y> bool maximize(X &a, Y b){
+    if (a < b) return a = b, true;
+    return false;
+}
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+int rnd(int l, int r) {return l + rng() % (r - l + 1);}
+const int N = 1e6 + 5;
+ 
+int n, m;
+vector<vector<int>> a;
+ 
+int ktra(int mid){
+    vector<vector<int>> f;
+    f = vector(n + 5, vector<int>(m + 5, 0));
+ 
+    int ans = 0;
+    For(i,1,n)  For(j,1,m){
+        int x = (a[i][j] >= mid);
+        if(x == 0){
+            f[i][j] = 0;
+        }else{
+            f[i][j] = min({f[i-1][j-1], f[i-1][j], f[i][j-1]}) + 1;
+        }
+        maximize(ans, f[i][j]);
+    }
+    return ans >= mid;
+}
+ 
+void solve(){
+    cin >> n >> m;
+    a = vector(n + 5, vector<int>(m + 5, 0));
+    For(i,1,n)  For(j,1,m) cin >> a[i][j];
+ 
+    int s = min(n, m);
+    int l = 1, r = s;
+ 
+    int ans = 1;
+    while(l <= r){
+        int mid = (l + r) >> 1;
+        if(ktra(mid)){
+            ans = mid;
+            l = mid + 1;
+        }else{
+            r = mid - 1;
+        }
+    }
+    cout << ans << "\n";
+}
+ 
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    #define TASK ""
+    if(fopen(TASK".inp", "r")) {
+        freopen(TASK".inp", "r", stdin);
+        freopen(TASK".out", "w", stdout);
+    }
+    int T = 1;
+    cin >> T;
+    while(T--){
+        solve();
+    }
+    return 0;
+}
