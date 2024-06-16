83848fe21f2553e1ccafd6d6c4de528f52439794
Jack1eNgBaro <118097208+Jack1eNgBaro@users.noreply.github.com>
2024-06-16 20:49:53 +0700

    Committing files

---
+++ CodeForces/C++20 (GCC 11-64)/579A | Raising Bacteria/184426586.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main(){
+// freopen("input.txt", "r", stdin);
+// freopen("output.txt", "w", stdout);
+ 
+ std::ios::sync_with_stdio(0);
+ cin.tie(0); cout.tie(0);
+   
+ int n; cin >> n;
+ cout << __builtin_popcount(n);  
+   
+ return 0;
+}
