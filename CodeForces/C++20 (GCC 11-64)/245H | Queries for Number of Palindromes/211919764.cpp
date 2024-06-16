be0b26b1b56de1e4472ec450116beb46e64e3f2d
Jack1eNgBaro <118097208+Jack1eNgBaro@users.noreply.github.com>
2024-06-16 19:56:26 +0700

    Committing files

---
+++ CodeForces/C++20 (GCC 11-64)/245H | Queries for Number of Palindromes/211919764.cpp
@@ -0,0 +1,81 @@
+#include<bits/stdc++.h>
+#define MOD 1000000007
+#define fi first
+#define se second
+#define ll long long
+#define ii pair<int,int>
+#define Dennis ""
+#define heap_max(a) priority_queue<a>
+#define heap_min(a) priority_queue<a, vector<a>, greater <a>>
+#define all(x) (x).begin(), (x).end()
+#define pb push_back
+#define eb emplace_back
+#define el cout << '\n'
+#define rep(i, n) for(int i = 0; i < (n); i++)
+#define For(i, a, b) for(int i = (a); i <= (b); i++)
+#define Fod(i, a, b) for(int i = (a); i >= (b); i--)
+ 
+using namespace std;
+template <class X, class Y> bool minimize(X &a, Y b){
+    if (a > b) return a = b, true;
+    return false;
+}
+template <class X, class Y> bool maximize(X &a, Y b){
+    if (a < b) return a = b, true;
+    return false;
+}
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+int rnd(int l, int r) {return l + rng() % (r - l + 1);}
+const int N = 5005;
+ 
+string s; 
+int dp[N][N], f[N][N];
+ 
+int get(int x, int y, int u, int v){
+    return f[u][v] - f[x-1][v] - f[u][y-1] + f[x-1][y-1];
+}
+ 
+void solve(){
+    memset(dp, 0, sizeof(dp));
+    cin >> s;
+    int n = s.size();
+    s = "~" + s;
+ 
+    For(i,1,n) dp[i][i] = f[i][i] = 1;
+    for(int len=2;len<=n;len++){
+        for(int i=1;i+len-1<=n;i++){
+            int j = i + len - 1;
+            if(len == 2){
+                dp[i][j] = (s[i] == s[j]);
+            }else{
+                dp[i][j] = (s[i] == s[j] && dp[i+1][j-1]);
+            }
+        }
+    }
+    For(i,1,n)  For(j,1,n) 
+        f[i][j] = f[i-1][j] + f[i][j-1] - f[i-1][j-1] + dp[i][j];
+ 
+    int q; cin >> q;
+    while(q--){
+        int l, r; cin >> l >> r;
+        if(l > r) swap(l, r);
+        cout << get(l, l, r, r) << "\n";
+    }
+ 
+}
+ 
+main(){
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    int bla = uniform_int_distribution<int>(1, 100)(rng);
+    #define TASK ""
+    if(fopen(TASK".inp", "r")) {
+        freopen(TASK".inp", "r", stdin);
+        freopen(TASK".out", "w", stdout);
+    }
+    int T = 1;
+    //cin >> T;
+    while(T--){
+        solve();
+    }
+    return 0;
+}
