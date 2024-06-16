1ccfe92d8d9ba61c1eee4cdcd81cd040d6b5f635
Jack1eNgBaro <118097208+Jack1eNgBaro@users.noreply.github.com>
2024-06-16 20:20:12 +0700

    Committing files

---
+++ CodeForces/C++14 (GCC 6-32)/677A | Vanya and Fence/193990624.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define ll long long
+#define pii pair<int,int>
+#define bit(x, i) ((x >> i) & 1)
+ 
+const int mod = 1e9 + 7;
+const int maxn = 1e6;
+const int base = 31;
+ 
+void file()
+{
+ ios_base::sync_with_stdio(0);
+ cin.tie(0); cout.tie(0);
+// freopen("input.txt", "r", stdin);
+// freopen("output.txt", "w", stdout);
+}
+ 
+int main()
+{
+ file();
+ int n, k; cin >> n >> k; 
+ int ans = 0;
+ while(n--){
+  int x; cin >> x;
+  if(x > k) ans += 2;
+  else ans++;
+ }
+ cout << ans;
+ return 0;
+}
