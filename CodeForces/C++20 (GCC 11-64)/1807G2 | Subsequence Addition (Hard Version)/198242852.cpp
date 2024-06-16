c7890c530b43c3d148e00f5738de147c84de232d
Jack1eNgBaro <118097208+Jack1eNgBaro@users.noreply.github.com>
2024-06-16 20:14:44 +0700

    Committing files

---
+++ CodeForces/C++20 (GCC 11-64)/1807G2 | Subsequence Addition (Hard Version)/198242852.cpp
@@ -0,0 +1,41 @@
+/*
+  "There is no light without dark
+  There is no joy without pain
+  You can't have a rainbow without a little rain"
+         ~ Andrew Tate
+ 
+*/
+ 
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define ll long long
+#define pii pair<int,int>
+#define bit(x, i) ((x >> i) & 1)
+ 
+const int mod = 1e9 + 7;
+const int maxn = 1e6;
+const int base = 31;
+ 
+void file()
+{
+ ios_base::sync_with_stdio(0);
+ cin.tie(0); cout.tie(0);
+// freopen("input.txt", "r", stdin);
+// freopen("output.txt", "w", stdout);
+}
+ 
+int main()
+{
+ file();
+ int n; cin >> n;
+ if(n&1) return cout << 0, 0;
+ int ans = 1, a = 2;
+ n/=2; 
+ while(n){
+  if(n&1) ans *= a;
+  a *= a; n >>= 1;
+ }
+ cout << ans;
+ return 0;
+}
