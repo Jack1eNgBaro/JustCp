e580e4451c3d22566eee5bb4542d311ee67c5851
Jack1eNgBaro <118097208+Jack1eNgBaro@users.noreply.github.com>
2024-06-16 19:57:44 +0700

    Committing files

---
+++ CodeForces/C++20 (GCC 11-64)/534A | Exam/211384930.cpp
@@ -0,0 +1,56 @@
+#include<bits/stdc++.h>
+#define MOD 1000000007
+#define fi first
+#define se second
+#define ll long long
+#define ii pair<int,int>
+#define Dennis ""
+#define heap_max(a) priority_queue<a>
+#define heap_min(a) priority_queue<a, vector<a>, greater <a>>
+#define all(x) (x).begin(), (x).end()
+#define pb push_back
+#define eb emplace_back
+#define el cout << '\n'
+#define rep(i, n) for(int i = 0; i < (n); i++)
+#define For(i, a, b) for(int i = (a); i <= (b); i++)
+#define Fod(i, a, b) for(int i = (a); i >= (b); i--)
+ 
+using namespace std;
+template <class X, class Y> bool minimize(X &a, Y b){
+    if (a > b) return a = b, true;
+    return false;
+}
+template <class X, class Y> bool maximize(X &a, Y b){
+    if (a < b) return a = b, true;
+    return false;
+}
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+int rnd(int l, int r) {return l + rng() % (r - l + 1);}
+const int N = 1e5 + 5;
+ 
+void solve(){
+ int n; cin >> n;
+ if(n <= 2) cout << "1\n1";
+ else if(n == 3) cout << "2\n1 3";
+ else if(n == 4) cout << "4\n3 1 4 2";
+ else{
+  cout << n << "\n";
+  for(int i=1;i<=n;i+=2) cout << i << " ";
+  for(int i=2;i<=n;i+=2) cout << i << " ";   
+ }
+}
+ 
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    int bla = uniform_int_distribution<int>(1, 100)(rng);
+    #define TASK ""
+    if(fopen(TASK".inp", "r")) {
+        freopen(TASK".inp", "r", stdin);
+        freopen(TASK".out", "w", stdout);
+    }
+    int T = 1;
+    while(T--){
+        solve();
+    }
+    return 0;
+}
