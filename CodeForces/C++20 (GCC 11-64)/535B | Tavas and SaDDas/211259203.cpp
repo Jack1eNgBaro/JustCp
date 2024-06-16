6d0d80189655c47179b79daf32c1e18ffde1ca7b
Jack1eNgBaro <118097208+Jack1eNgBaro@users.noreply.github.com>
2024-06-16 19:58:30 +0700

    Committing files

---
+++ CodeForces/C++20 (GCC 11-64)/535B | Tavas and SaDDas/211259203.cpp
@@ -0,0 +1,65 @@
+#include<bits/stdc++.h>
+#define MOD 1000000007
+#define fi first
+#define se second
+#define int long long
+#define ii pair<int,int>
+#define Dennis ""
+#define heap_max(a) priority_queue<a>
+#define heap_min(a) priority_queue<a, vector<a>, greater <a>>
+#define all(x) (x).begin(), (x).end()
+#define pb push_back
+#define eb emplace_back
+#define el cout << '\n'
+#define rep(i, n) for(int i = 0; i < (n); i++)
+#define For(i, a, b) for(int i = (a); i <= (b); i++)
+#define Fod(i, a, b) for(int i = (a); i >= (b); i--)
+ 
+using namespace std;
+template <class X, class Y> bool minimize(X &a, Y b){
+    if (a > b) return a = b, true;
+    return false;
+}
+template <class X, class Y> bool maximize(X &a, Y b){
+    if (a < b) return a = b, true;
+    return false;
+}
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+int rnd(int l, int r) {return l + rng() % (r - l + 1);}
+const int N = 1e5 + 5;
+ 
+vector<int> v;
+void init(){
+ queue<int> q;
+ q.push(4); q.push(7);
+ while(q.size()){
+  int u = q.front(); q.pop();
+  if(u >= 1e10) break;
+  v.pb(u);
+  q.push(u*10+4); 
+  q.push(u*10+7);
+ }
+}
+ 
+void solve(){
+    init();
+    int n; cin >> n;
+    auto it = lower_bound(v.begin(), v.end(), n) - v.begin() + 1;
+    cout << it;
+}
+ 
+main(){
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    int bla = uniform_int_distribution<int>(1, 100)(rng);
+    #define TASK ""
+    if(fopen(TASK".inp", "r")) {
+        freopen(TASK".inp", "r", stdin);
+        freopen(TASK".out", "w", stdout);
+    }
+    int T = 1;
+    //cin >> T;
+    while(T--){
+        solve();
+    }
+    return 0;
+}
