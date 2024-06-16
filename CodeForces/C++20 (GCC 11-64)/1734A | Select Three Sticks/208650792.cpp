47aabd5ae0d51122f6c2a878c32983b8f331fe7c
Jack1eNgBaro <118097208+Jack1eNgBaro@users.noreply.github.com>
2024-06-16 20:02:28 +0700

    Committing files

---
+++ CodeForces/C++20 (GCC 11-64)/1734A | Select Three Sticks/208650792.cpp
@@ -0,0 +1,46 @@
+/*
+  The only thing we have to fear is fear itself.
+*/
+ 
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define ll long long
+#define pii pair<int,int>
+#define bit(x, i) ((x >> i) & 1)
+#define fi first
+#define se second
+#define vi vector<int> 
+ 
+const int mod = 1e9 + 7;
+const int maxn = 1e6;
+const int base = 31;
+ 
+void file()
+{
+ ios_base::sync_with_stdio(0);
+ cin.tie(0); cout.tie(0);
+// freopen("input.txt", "r", stdin);
+// freopen("output.txt", "w", stdout);
+}
+ 
+int main()
+{
+ file();
+ int tc; cin >> tc;
+ while(tc--){
+  int n; cin >> n;
+  int a[n + 1]; 
+  for(int i=1;i<=n;i++) cin >> a[i];
+ 
+  sort(a + 1, a + n + 1);
+  int ans =1e9;
+  for(int i=1;i+2<=n;i++){
+   int mx = a[i+1];
+   int sum = mx - a[i] + a[i+2] - mx;
+   ans = min(ans, sum);   
+  }
+  cout << ans << "\n";
+ }
+ return 0;
+}
