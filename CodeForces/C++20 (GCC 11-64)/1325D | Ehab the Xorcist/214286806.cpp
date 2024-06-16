ba518d35cf9b15434c095070063c88a99074ddee
Jack1eNgBaro <118097208+Jack1eNgBaro@users.noreply.github.com>
2024-06-16 19:50:10 +0700

    Committing files

---
+++ CodeForces/C++20 (GCC 11-64)/1325D | Ehab the Xorcist/214286806.cpp
@@ -0,0 +1,62 @@
+#include<bits/stdc++.h>
+#define MOD 1000000007
+#define fi first
+#define se second
+#define int long long
+#define ii pair<int,int>
+#define Dennis ""
+#define heap_max(a) priority_queue<a>
+#define heap_min(a) priority_queue<a, vector<a>, greater <a>>
+#define all(x) (x).begin(), (x).end()
+#define pb push_back
+#define eb emplace_back
+#define el cout << '\n'
+#define rep(i, n) for(int i = 0; i < (n); i++)
+#define For(i, a, b) for(int i = (a); i <= (b); i++)
+#define Fod(i, a, b) for(int i = (a); i >= (b); i--)
+#define bit(x, i) ((x >> i) & 1)
+ 
+using namespace std;
+template <class X, class Y> bool minimize(X &a, Y b){
+    if (a > b) return a = b, true;
+    return false;
+}
+template <class X, class Y> bool maximize(X &a, Y b){
+    if (a < b) return a = b, true;
+    return false;
+}
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+int rnd(int l, int r) {return l + rng() % (r - l + 1);}
+const int N = 1e6 + 5;
+ 
+void solve(){
+    int u, v; cin >> u >> v;
+    if(u > v) return cout << "-1", void();
+    if(v == 0) return cout << "0", void();
+    if(u == v) return cout << "1\n" << u, void();
+    if((v-u)&1) return cout << "-1\n", void();
+ 
+    int x = (v - u) / 2;
+    if(u&x){
+     cout << "3\n" << u << ' ' << x << ' ' << x << "\n";
+    }else{
+     cout << "2\n" << (u^x) << " " << x;
+    }
+ 
+}
+ 
+main(){
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    int bla = uniform_int_distribution<int>(1, 100)(rng);
+    #define TASK ""
+    if(fopen(TASK".inp", "r")) {
+        freopen(TASK".inp", "r", stdin);
+        freopen(TASK".out", "w", stdout);
+    }
+    int T = 1;
+    //cin >> T;
+    while(T--){
+        solve();
+    }
+    return 0;
+}
