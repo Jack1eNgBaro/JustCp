ac8dbff1acb1f224fc23177ef2d057d99d7377cf
Jack1eNgBaro <118097208+Jack1eNgBaro@users.noreply.github.com>
2024-06-16 19:32:10 +0700

    Committing files

---
+++ CodeForces/C++20 (GCC 11-64)/1873E | Building an Aquarium/224392313.cpp
@@ -0,0 +1,80 @@
+#include<bits/stdc++.h>
+ 
+////Ta thuat
+//#pragma GCC optimize("O2")
+//#pragma GCC target("avx,avx2,fma")
+ 
+#define MOD 1000000007
+#define fi first
+#define se second
+#define int long long
+#define ii pair<int,int>
+#define Dennis ""
+#define heap_max(a) priority_queue<a>
+#define heap_min(a) priority_queue<a, vector<a>, greater <a>>
+#define all(x) (x).begin(), (x).end()
+#define pb push_back
+#define eb emplace_back
+#define el cout << '\n'
+#define rep(i, n) for(int i = 0; i < (n); i++)
+#define For(i, a, b) for(int i = (a); i <= (b); i++)
+#define Fod(i, a, b) for(int i = (a); i >= (b); i--)
+#define bit(x, i) ((x >> i) & 1)
+ 
+using namespace std;
+template <class X, class Y> bool minimize(X &a, Y b){
+    if (a > b) return a = b, true;
+    return false;
+}
+template <class X, class Y> bool maximize(X &a, Y b){
+    if (a < b) return a = b, true;
+    return false;
+}
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+int rnd(int l, int r) {return l + rng() % (r - l + 1);}
+const int N = 1e6 + 5;
+ 
+int n, k, a[N];
+int kt(int mid){
+ int cnt = 0;
+ For(i,1,n){
+  if(mid >= a[i]) cnt += (mid - a[i]);
+ }
+ return cnt <= k;
+}
+ 
+void solve(){
+    cin >> n >> k;
+    For(i,1,n) cin >> a[i];
+ 
+    // cout << n << ' ' << k << "\n";
+    // For(i,1,n) cout << a[i] << ' '; cout << "\n";
+ 
+    int ans = -1;
+    // return;
+    int left = 1, right = 2e9;
+    while(left <= right){
+     int mid = left + right >> 1;
+     if(kt(mid)){
+      ans = mid;
+      left = mid + 1;
+     }else right = mid - 1;
+    }
+    cout << ans << "\n";
+}
+ 
+main(){
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    int bla = uniform_int_distribution<int>(1, 100)(rng);
+    #define TASK ""
+    if(fopen(TASK".inp", "r")) {
+        freopen(TASK".inp", "r", stdin);
+        freopen(TASK".out", "w", stdout);
+    }
+    int T = 1;
+    cin >> T;
+    while(T--){
+        solve();
+    }
+    return 0;
+}
