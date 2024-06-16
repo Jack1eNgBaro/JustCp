78b54c5ea205824a465f66a8966029ca53274609
Jack1eNgBaro <118097208+Jack1eNgBaro@users.noreply.github.com>
2024-06-16 19:55:12 +0700

    Committing files

---
+++ CodeForces/C++20 (GCC 11-64)/1847A | The Man who became a God /212392484.cpp
@@ -0,0 +1,67 @@
+#include<bits/stdc++.h>
+#define MOD 1000000007
+#define fi first
+#define se second
+#define ll long long
+#define ii pair<int,int>
+#define Dennis ""
+#define heap_max(a) priority_queue<a>
+#define heap_min(a) priority_queue<a, vector<a>, greater <a>>
+#define all(x) (x).begin(), (x).end()
+#define pb push_back
+#define eb emplace_back
+#define el cout << '\n'
+#define rep(i, n) for(int i = 0; i < (n); i++)
+#define For(i, a, b) for(int i = (a); i <= (b); i++)
+#define Fod(i, a, b) for(int i = (a); i >= (b); i--)
+ 
+using namespace std;
+template <class X, class Y> bool minimize(X &a, Y b){
+    if (a > b) return a = b, true;
+    return false;
+}
+template <class X, class Y> bool maximize(X &a, Y b){
+    if (a < b) return a = b, true;
+    return false;
+}
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+int rnd(int l, int r) {return l + rng() % (r - l + 1);}
+const int N = 105;
+ 
+int a[N], n, k;
+ 
+void reset(){
+    memset(a, 0, sizeof(a));
+}
+ 
+void solve(){
+    reset();
+    cin >> n >> k;
+    For(i,1,n) cin >> a[i];
+ 
+    vector<int> v;
+    For(i,2,n) v.pb(abs(a[i] - a[i-1]));
+    sort(v.begin(), v.end());
+ 
+    // for(auto x: v) cout << x << ' '; cout << "\n";
+ 
+    int ans = 0;
+    For(i,0,n-k-1) ans += v[i];
+    cout << ans << "\n";
+}
+ 
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    int bla = uniform_int_distribution<int>(1, 100)(rng);
+    #define TASK ""
+    if(fopen(TASK".inp", "r")) {
+        freopen(TASK".inp", "r", stdin);
+        freopen(TASK".out", "w", stdout);
+    }
+    int T = 1;
+    cin >> T;
+    while(T--){
+        solve();
+    }
+    return 0;
+}
